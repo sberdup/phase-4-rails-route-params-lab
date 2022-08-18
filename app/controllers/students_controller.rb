a1e28c25eee0358e6039944611de9bdf62dccd81
Sergio Berdeja <sergioberdeja@gmail.com>
2022-08-17 18:38:02 -0600

    Completed assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,15 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+    if params[:name] != nil
+      # students = Student.where("first_name = ? OR last_name = ?", params[:name], params[:name])
+      students = students.filter{|student| [student.first_name.downcase, student.last_name.downcase].include?(params[:name].downcase)}
+    end
     render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student 
+  end
 end
